955b296543763d37eff62b30879d2e6709c359ec
Gabriel Giordano <gabriel.giordano@estudar.org.br>
2016-11-14 17:12:18 -0200

    Fix null pointer in list projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,10 +6,10 @@ require "projects/policy"
 
 class ProjectsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action -> { authorize_user_with(Projects::Policy, current_user.id, project_id, action_name) }
+  before_action -> { authorize_user_with(Projects::Policy, current_user&.id, project_id, action_name) }
 
   def index
-    @projects = Projects::List.execute(current_user.id)
+    @projects = Projects::List.execute(current_user&.id)
   end
 
   def new
